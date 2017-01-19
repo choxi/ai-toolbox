1a686c25ddc03877ae72c7ad7c71a64d14b6b8a9
Roshan Choxi <roshan.choxi@gmail.com>
2017-01-19 13:39:27 -0800

    show commit activity

--- app/models/repo.rb
+++ app/models/repo.rb
@@ -5,12 +5,17 @@ class Repo < ApplicationRecord
       repo: self.name
     })
 
+    commit_activity = Github.repos.stats.commit_activity({
+      user: self.user, repo: self.name
+    })
+
     self.update_attributes!({
       forks_count: github_repo.forks_count,
       stargazers_count: github_repo.stargazers_count,
       watchers_count: github_repo.watchers_count,
       subscribers_count: github_repo.subscribers_count,
-      github_created_at: github_repo.created_at
+      github_created_at: github_repo.created_at,
+      commit_activity: commit_activity.to_json
     })
   end
 
@@ -22,5 +27,9 @@ class Repo < ApplicationRecord
         puts e
       end
     end
+  end
+
+  def commit_activity
+    @json_commit_activity ||= JSON.parse(self[:commit_activity])
   end
 end
