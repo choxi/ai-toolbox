a2e9c44c464b74716d02ae702416965fa844e062
Roshan Choxi <roshan.choxi@gmail.com>
2017-01-19 12:09:45 -0800

    implement Repo#update_stats

--- app/models/repo.rb
+++ app/models/repo.rb
@@ -1,3 +1,26 @@
 class Repo < ApplicationRecord
+  def update_stats
+    github_repo = Github.repos.get({
+      user: self.user,
+      repo: self.name
+    })
 
+    self.update_attributes!({
+      forks_count: github_repo.forks_count,
+      stargazers_count: github_repo.stargazers_count,
+      watchers_count: github_repo.watchers_count,
+      subscribers_count: github_repo.subscribers_count,
+      github_created_at: github_repo.created_at
+    })
+  end
+
+  def self.update_stats
+    Repo.all.each do |repo|
+      begin
+        repo.update_stats
+      rescue => e
+        puts e
+      end
+    end
+  end
 end
