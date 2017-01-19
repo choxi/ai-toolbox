54cefb5b17bd4d06aad468f568cac296c74d954d
Roshan Choxi <roshan.choxi@gmail.com>
2017-01-19 11:22:39 -0800

    add model + seeds

---
+++ app/models/repo.rb
@@ -0,0 +1,3 @@
+class Repo < ApplicationRecord
+
+end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,18 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+repos = Repo.create!([
+  {
+    name: "tensorflow",
+    user: "tensorflow"
+  },
+  {
+    name: "keras",
+    user: "fchollet"
+  },
+  {
+    name: "neural-talk",
+    user: "karpathy"
+  }
+])
